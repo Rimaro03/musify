57adc8a2fbe840ab3e33ad4821b0f6b61b61a35a
leonardo <leonardoongaro4@gmail.com>
2025-07-22 14:50:14 +0200

    added filters and header to library fragment

--- app/src/main/java/com/rimaro/musify/ui/fragments/library/LibraryFragment.kt
+++ app/src/main/java/com/rimaro/musify/ui/fragments/library/LibraryFragment.kt
@@ -7,9 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.rimaro.musify.data.remote.model.SimplifiedPlaylistObject
 import com.rimaro.musify.databinding.FragmentLibraryBinding
+import com.rimaro.musify.ui.adapters.LibraryFilterAdapter
+import com.rimaro.musify.ui.adapters.LibraryHeaderAdapter
 import com.rimaro.musify.ui.adapters.PlaylistAdapter
 import com.rimaro.musify.ui.fragments.home.HomeFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
@@ -48,7 +51,10 @@ class LibraryFragment : Fragment() {
         }
 
         val playlistAdapter = PlaylistAdapter(::navigateAction)
-        trackRecyclerView.adapter = playlistAdapter
+        val libraryFilterAdapter = LibraryFilterAdapter()
+        val libraryHeaderAdapter = LibraryHeaderAdapter()
+        val concatAdapter = ConcatAdapter(libraryFilterAdapter, libraryHeaderAdapter, playlistAdapter)
+        trackRecyclerView.adapter = concatAdapter
 
         viewModel.userPlaylists.observe(viewLifecycleOwner) {
             playlistAdapter.submitList(it)
